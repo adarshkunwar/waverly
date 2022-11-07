a56a1fc75092f2ef4f45bc9ca29e886d4c338ca4
Ankit Bhandari <bhandariankit2075@gmail.com>
2022-11-07 20:48:28 +0545

    landing UI changes

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import img from "../img/waverly default.png";
 const Nav = (props) => {
-  // chnage this accordingly. Make it props or wahtever you wish
+  // change this accordingly. Make it props or wahtever you wish
   let loggedIn = false;
   //   this will change later on
 
   return (
-    <div className="absolute w-[40rem] -mt-8">
+    <div className="absolute w-[40rem]  navbar">
       <div className="flex justify-between">
         <div>
-          <img src={img} alt="" className="w-36 h-36" />
+          <img src={img} alt="" className="w-39 h-12" />
         </div>
       </div>
       {/* {activeTab} */}
