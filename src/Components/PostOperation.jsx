7bda03ad83a187e7ce22d7158af0937b7f79a541
Ankit Bhandari <bhandariankit2075@gmail.com>
2022-11-10 00:22:54 +0545

    some btns

--- src/Components/PostOperation.jsx
+++ src/Components/PostOperation.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
+import { BiImageAdd } from "react-icons/bi";
+import { IconContext } from "react-icons";
+
 const PostOperation = () => {
-  return <div>PostOperation</div>;
+  return <div>
+      <div>
+        <button className="absolute bottom-1/2 left-52 btn focus:outline-none bg-[#efefef] bigbtn">PREVIEW</button>
+
+        <button  className="logout mr-5  scale-90">
+                <IconContext.Provider value={{ color: "red", size: "27px" }}>
+                  <BiImageAdd style={{ size: "200px" }} />
+                </IconContext.Provider>
+              </button>
+      </div>
+  </div>;
 }
 
 export default PostOperation
