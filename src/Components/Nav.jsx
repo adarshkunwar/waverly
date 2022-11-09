e549a543943735ff7a9d30ba6e684200b8197d18
Ankit Bhandari <bhandariankit2075@gmail.com>
2022-11-09 16:30:21 +0545

    handleLogout on nav btn

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import img from "../img/waverly default.png";
 import { CgLogOff } from 'react-icons/cg';
 import { IconContext } from "react-icons";
+import Deso from 'deso-protocol';
 const Nav = ({ logIn }) => {
   // chnage this accordingly. Make it props or wahtever you wish
   //   this will change later on
+
+  const handleLogOut = async () => {
+    const deso = new Deso();
+    const request = localStorage.getItem("deso_user_key");
+    const response = await deso.identity.logout(request);
+    localStorage.setItem('isLoggedIn', !response);
+    // history.push("/");
+    window.location.reload();
+  }
 
   return (
     <div className="absolute w-[40rem] navbar  ">
@@ -13,7 +23,7 @@ const Nav = ({ logIn }) => {
           <img src={img} alt="" className="w-36 h-30 mt-1" />
         </nav>
       
-      {logIn?(<button className="logout mr-5 mt-1.5 scale-90" >
+      {logIn?(<button onClick={handleLogOut} className="logout mr-5 mt-1.5 scale-90" >
           <IconContext.Provider value={{color: "red", size: "27px"}}>
           <CgLogOff style={{size: '200px'}}/>
           </IconContext.Provider>
