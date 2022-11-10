901007fc1377415310c18f8913db6abc3cb51a43
Ankit Bhandari <bhandariankit2075@gmail.com>
2022-11-10 14:11:18 +0545

    needed elem for preview post page

--- src/Components/PostOperation.jsx
+++ src/Components/PostOperation.jsx
@@ -2,17 +2,34 @@ import React from 'react'
 
 import { BiImageAdd } from "react-icons/bi";
 import { IconContext } from "react-icons";
+import {HiOutlineExternalLink} from 'react-icons/hi';
+import {ImEmbed} from 'react-icons/im';
 
 const PostOperation = () => {
   return <div>
       <div>
+      <textarea id="textbox" className='textbox' rows="10" cols="50"></textarea>
+      <input type='text'  className='textbox'></input>
         <button className="absolute bottom-1/2 left-52 btn focus:outline-none bg-[#efefef] bigbtn">PREVIEW</button>
 
         <button  className="logout mr-5  scale-90">
                 <IconContext.Provider value={{ color: "red", size: "27px" }}>
                   <BiImageAdd style={{ size: "200px" }} />
                 </IconContext.Provider>
+              </button> 
+               <button  className="logout mr-5  scale-90">
+                <IconContext.Provider value={{ color: "red", size: "27px" }}>
+                  <HiOutlineExternalLink style={{ size: "200px" }} />
+                </IconContext.Provider>
               </button>
+
+              <button  className="logout mr-5  scale-90">
+                <IconContext.Provider value={{ color: "red", size: "27px" }}>
+                  <ImEmbed style={{ size: "200px" }} />
+                </IconContext.Provider>
+              </button> 
+
+             
       </div>
   </div>;
 }
