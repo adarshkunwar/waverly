9ad3c612cafff8fa4229c2a04f4051fee9a30670
yogeshAryal <yogesh.772417@thc.tu.edu.np>
2022-11-10 21:44:37 +0545

    upload image integrated

--- src/Components/PostOperation.jsx
+++ src/Components/PostOperation.jsx
@@ -1,15 +1,34 @@
 // comment from alex, embedded btn still have work to do. only the design is done
 
 import React, { useState } from "react";
-
+import Deso from 'deso-protocol';
 import { BiImageAdd } from "react-icons/bi";
 import { IconContext } from "react-icons";
 import { HiOutlineExternalLink } from "react-icons/hi";
 import { ImEmbed } from "react-icons/im";
 import EmbeddBtn from "./EmbeddBtn";
-
 const PostOperation = () => {
   const [textBoxActive, setTextBoxActive] = useState(false);
+  const [imgAddColor  , setImgAddColor  ] = useState("red");
+  const imgURLs = [];
+  const [bodyText, setBodyText] = useState("");
+
+  const handleUploadImage = async()=>{
+    try {
+      console.log("upload image clicked");
+      console.log(bodyText);
+      const pub_key = localStorage.getItem("user_key")
+      const deso = new Deso();
+      const request = {
+        "UserPublicKeyBase58Check": pub_key
+      };
+      const response = await deso.media.uploadImage(request);
+      imgURLs.push(response.ImageURL);
+      setImgAddColor("green");
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     <div>
       <div>
@@ -19,6 +38,8 @@ const PostOperation = () => {
           className="textbox border-2 resize-none bg-slate-300 w-[37rem] mt-3 px-5 mx-5 focus:outline-none"
           rows="7"
           cols="50"
+          value={bodyText}
+          onChange={(e)=>setBodyText(e.target.value)}
         ></textarea>
         {/* btn start here */}
         <div className="buttons mt-2 flex justify-between">
@@ -26,8 +47,8 @@ const PostOperation = () => {
           <div className="left-buttons flex">
             <div className="img-upload">
               <button className="logout mr-5  scale-75">
-                <IconContext.Provider value={{ color: "red", size: "27px" }}>
-                  <BiImageAdd style={{ size: "200px" }} />
+                <IconContext.Provider value={{ color: `${imgAddColor}`, size: "27px" }}>
+                  <BiImageAdd style={{ size: "200px" }} onClick={handleUploadImage}/>
                 </IconContext.Provider>
               </button>
             </div>
