4770ce2e95ee96e58f5bef3535e44d09f8ee1287
Ankit Bhandari <bhandariankit2075@gmail.com>
2022-11-12 18:09:17 +0545

    nft small UI changes

--- src/Components/MintOperation.jsx
+++ src/Components/MintOperation.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { RiImageAddFill } from "react-icons/ri";
+import { IconContext } from "react-icons";
 
 const MintOperation = () => {
-  return <div>MintOperation</div>;
+  return (
+    <div className="flex">
+      <div>
+        <textarea
+          id="textbox"
+          className="rounded-xl textbox border-2 resize-none text-lg pt-2 bg-[#efefef] w-[17.5rem] mt-4 px-5 mx-5 focus:outline-none"
+          rows="5"
+          cols="1"
+          placeholder="Enter your post"
+        ></textarea>
+        <div className="flex">
+          <div className="img-upload">
+            <button className="logout mr-5  scale-75">
+              <IconContext.Provider value={{ size: "27px" }}>
+                <RiImageAddFill style={{ size: "200px" }} />
+              </IconContext.Provider>
+            </button>
+          </div>
+          <div>Number of copies</div>
+        </div>
+      </div>
+
+      <div>secondy text</div>
+    </div>
+  );
 };
 
 export default MintOperation;
+
